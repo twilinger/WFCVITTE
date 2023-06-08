f9f215426c2709238e553cbdefd7640e3cb17603
twilinger <rafaelasdeveloper@gmail.com>
2023-06-08 13:49:55 +0300

    value manager
    класс который из инпута(ака тайлмап) создаст для нас JaggedArray из значений каждого тайла, чтобы мы могли в дальнейшем работать конкретно с этим JaggedArray

--- TEST.cs
+++ TEST.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 using WaveFunctionCollapse;
@@ -12,13 +13,31 @@ public class TEST : MonoBehaviour
     {
         InputReader reader = new InputReader(input);
         var grid = reader.ReadInputToGrid();
+        //for (int row = 0; row < grid.Length; row++)
+        //{
+        //    for (int col = 0; col < grid[0].Length; col++)
+        //    {
+        //        Debug.Log("Row: " + row + "Col:" + col + "tile name" + grid[row][col].Value.name);
+        //    }
+        //}
+        ValuesManager<TileBase> valueManager = new ValuesManager<TileBase>(grid);
+        StringBuilder builder = null;
+        List<string> list = new List<string>();
         for (int row = 0; row < grid.Length; row++)
         {
+            builder = new StringBuilder();
             for (int col = 0; col < grid[0].Length; col++)
             {
-                Debug.Log("Row: " + row + "Col:" + col + "tile name" + grid[row][col].Value.name);
+                builder.Append(valueManager.GetGridValuesIncludingOffset(col, row) + " ");
             }
+            list.Add(builder.ToString());
         }
+        list.Reverse();
+        foreach(var item in list)
+        {
+            Debug.Log(item);
+        }
+
     }
 
     // Update is called once per frame
